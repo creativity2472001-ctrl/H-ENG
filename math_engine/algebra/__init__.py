4fe8c9268a1eeb23c5c93d22d79b066b0687daca
creativity2472001-ctrl <creativity2472001@gmail.com>
2026-03-09 04:30:15 +0200

    Create __init__.py

---
+++ math_engine/algebra/__init__.py
@@ -0,0 +1,10 @@
+# math_engine/algebra/__init__.py
+from .algebra_part1 import CompleteAlgebraSolver
+from .algebra_part2 import IntermediateAlgebraSolver
+from .algebra_part3 import AdvancedAlgebraSolver
+
+__all__ = [
+    'CompleteAlgebraSolver',
+    'IntermediateAlgebraSolver',
+    'AdvancedAlgebraSolver'
+]
