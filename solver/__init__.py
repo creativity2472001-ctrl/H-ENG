4827f18ef49541bf68e65db28c02af341601e246
creativity2472001-ctrl <creativity2472001@gmail.com>
2026-03-09 04:22:22 +0200

    Update __init__.py

--- solver/__init__.py
+++ solver/__init__.py
@@ -1,1 +1,10 @@
-
+# solver/__init__.py
+from .solver_controller import (
+    solve_calculate,
+    solve_template,
+    solve_system,
+    search_templates,
+    list_templates,
+    get_stats
+)
+from .steps_engine import solve_with_steps
