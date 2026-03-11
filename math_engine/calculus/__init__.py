f629d602c582f5aba88fe92e2dc569acf65ef761
creativity2472001-ctrl <creativity2472001@gmail.com>
2026-03-12 00:48:46 +0200

    Update __init__.py

--- math_engine/calculus/__init__.py
+++ math_engine/calculus/__init__.py
@@ -1,1 +1,15 @@
+"""
+================================================================================
+مجلد التفاضل والتكامل - Calculus Package
+================================================================================
+"""
 
+from .calculus_part1 import CalculusSolverPart1
+from .calculus_part2 import CalculusSolverPart2
+from .calculus_part3 import CalculusSolverPart3
+
+__all__ = [
+    'CalculusSolverPart1',
+    'CalculusSolverPart2',
+    'CalculusSolverPart3'
+]
